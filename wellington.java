8d66f2ba001b459cb89442e288b9bb95c3bc0680
Ellie Leitner <leitnerellie@gmail.com>
2019-03-13 13:39:50 -0700

    updated the comment at the top of Wellington to be bold.

--- wellington.java
+++ wellington.java
@@ -1,6 +1,6 @@
 
 /**
- * Write a description of class wellington here.
+ * WWEEEEEEEEEEEEEEEEELLLLLLLLLLLLLLIIIIIINNNNNNNGGGGGGTOOOOONNNNN!!!!
  *
  * @author (your name)
  * @version (a version number or a date)
